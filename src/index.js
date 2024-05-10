dec5c5fd768c564f3e1aa31c62940c0b6c086258
viswanadh05 <viswanadh.bhaskarla@gmail.com>
2024-05-10 23:42:05 +1000

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
+import awsmobile from './aws-exports';
 
-Amplify.configure(config);
+Amplify.configure(awsmobile);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
